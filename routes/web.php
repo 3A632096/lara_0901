79bf06e39c827d5e92e2ea22517a9639701ce289
Yu Qin <zxas5820@yahoo.com.tw>
2019-11-11 21:40:55 +0800

    修改route以免資料找不到patch route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'admin'], function() {
 
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
-    Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+    Route::get('posts/{id}', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
     Route::post('posts/post',['as'=>'admin.posts.store','uses'=> 'AdminPostsController@store']);
-
+    Route::patch('posts/admin/posts/{id}',['as'=>'admin.posts.update','uses'=>'AdminPostsController@update']);
 });
